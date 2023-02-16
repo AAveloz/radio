4eb8667e6c96e2c696dcc353d1458c995cc00683
AAveloz <122534379+AAveloz@users.noreply.github.com>
2023-02-16 13:50:21 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showString("" + (receivedNumber))
+input.onButtonPressed(Button.A, function () {
+    x += 1
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(x * 10 + y)
+    radio.sendString("JuanMa")
+    basic.clearScreen()
+    radio.setGroup(1)
+    y = 0
+    x = 0
+})
+radio.onReceivedString(function (receivedString) {
+    led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 1)))
+})
+input.onButtonPressed(Button.B, function () {
+    y += 1
+})
+let x = 0
+let y = 0
 radio.setGroup(1)
+y = 0
+x = 0
 basic.forever(function () {
 	
 })
